183b8ff74afb20885dbf9732b0ae4fa46de629f3
guilatrova <guilhermelatrova@hotmail.com>
2017-04-08 08:36:14 -0300

    set staging and prod envs for Heroku

---
+++ fixdin/settings/local.py
@@ -0,0 +1,16 @@
+from fixdin.settings.base import *
+
+DEBUG = True
+
+SECRET_KEY = 'ji$1&jbw!u2d)6a&^zj#8&#gu1%eg$31y#&)@#y2%5h9vlwl^%'
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
---
+++ fixdin/settings/production.py
@@ -0,0 +1,18 @@
+# See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
+import os
+import dj_database_url
+from fixdin.settings.base import *
+
+SECRET_KEY = os.environ['SECRET_KEY']
+DEBUG = False
+
+# Databases
+DATABASES['default'] = dj_database_url.config()
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+#ADMINS = (('Guilherme Latrova','guilhermelatrova@hotmail.com'),)
