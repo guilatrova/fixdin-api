baddb71410c3af366884af273ed3ca273560b124
Guilherme Latrova <guilhermelatrova@hotmail.com>
2017-05-27 11:50:57 -0300

    Added CORS config

--- fixdin/settings/local.py
+++ fixdin/settings/local.py
@@ -4,7 +4,9 @@ DEBUG = True
 
 SECRET_KEY = 'ji$1&jbw!u2d)6a&^zj#8&#gu1%eg$31y#&)@#y2%5h9vlwl^%'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*'] 
+ 
+CORS_ORIGIN_ALLOW_ALL = True 
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
