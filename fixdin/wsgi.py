f31a64f8f5b71a3735545274122532f96ceea385
Guilherme Latrova <guilhermelatrova@hotmail.com>
2017-06-10 12:36:17 -0300

    fix module name in default settings config

--- fixdin/wsgi.py
+++ fixdin/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fixdin.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fixdin.settings.local")
 
 application = get_wsgi_application()
