0243ff2c8ee057f777264efe630fb591f41f2b19
Guilherme Latrova <guilhermelatrova@hotmail.com>
2017-08-20 14:21:23 -0300

    Renamed next expenses api view

--- reports/urls.py
+++ reports/urls.py
@@ -3,5 +3,5 @@ from reports import views
 
 urlpatterns = [
     url(r'^last-13-months/$', views.Last13MonthsAPIView.as_view(), name="last-13-months"),
-    url(r'^next-expenses/$', views.NextExpenses.as_view(), name="next-expenses")
+    url(r'^next-expenses/$', views.NextExpensesAPIView.as_view(), name="next-expenses")
 ]
