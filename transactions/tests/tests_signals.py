8d1cf2f212880e0e6e76af3c3fff563784de6a85
Guilherme Latrova <guilhermelatrova@hotmail.com>
2017-09-10 16:35:59 -0300

    Added test for delete periodic child

--- transactions/tests/tests_signals.py
+++ transactions/tests/tests_signals.py
@@ -14,7 +14,20 @@ class TransactionSignalsTestCase(TestCase, BaseTestHelper):
         self.account = self.create_account(self.user)
 
     def test_delete_parent_transaction_updates_all_children_to_next(self):
-        transactions = create_periodic_transactions(
+        transactions = self.create_periodic(4)
+
+        transactions[0].delete()
+        self.assertEqual(Transaction.objects.filter(periodic_transaction_id=transactions[0].id).count(), 0)
+        self.assertEqual(Transaction.objects.filter(periodic_transaction_id=transactions[1].id).count(), 3)
+
+    def test_delete_child_of_periodic_has_no_side_effect(self):
+        transactions = self.create_periodic(4)
+
+        transactions[1].delete()
+        self.assertEqual(Transaction.objects.filter(periodic_transaction_id=transactions[0].id).count(), 3)
+
+    def create_periodic(self, how_many):
+        return create_periodic_transactions(
             account=self.account,
             category=self.category,
             due_date=datetime.date(2017, 1, 1), 
@@ -25,9 +38,5 @@ class TransactionSignalsTestCase(TestCase, BaseTestHelper):
             periodic={
                 'period': 'daily',
                 'distance': 1,
-                'how_many': 4,
+                'how_many': how_many,
             })
-
-        transactions[0].delete()
-        self.assertEqual(Transaction.objects.filter(periodic_transaction_id=transactions[0].id).count(), 0)
-        self.assertEqual(Transaction.objects.filter(periodic_transaction_id=transactions[1].id).count(), 3)
