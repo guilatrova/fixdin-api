26bd1458a064515bf1c3e0a75e42401296678ad1
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-02-24 12:55:30 -0300

    Fixed report generator effective x real

--- reports/factories/LastMonthsReport.py
+++ reports/factories/LastMonthsReport.py
@@ -1,5 +1,5 @@
 from itertools import takewhile
-from django.db.models import Q, Sum, Case, When, F
+from django.db.models import Q, Sum, Case, When, F, Value, CharField
 from django.db.models.functions import Coalesce, TruncMonth, ExtractMonth, ExtractYear
 from datetime import datetime
 import calendar
@@ -41,35 +41,38 @@ class LastMonthsReportFactory:
 
     def _get_query(self):
         start_date = self.get_start_date()
-        sum_when = lambda **kwargs : Coalesce(Sum(Case(When(then=F('value'), **kwargs), default=0)), 0)
-        sum_kind = lambda kind : sum_when(kind=kind)
-        sum_kind_payed = lambda kind : sum_when(kind=kind, payment_date__month=F('date'))
 
         #TruncMonth cares about year too
         transactions = Transaction.objects.filter(
             Q(account__user_id=self.user_id),
             Q(due_date__gte=start_date) | Q(payment_date__gte=start_date)
         )
-        due = transactions.annotate(date=TruncMonth('due_date'))
+        due = transactions
         payed = transactions.filter(payment_date__isnull=False)\
             .exclude(
                 payment_date__month=ExtractMonth('due_date'),
                 payment_date__year=ExtractYear('due_date')
-                )\
-            .annotate(date=TruncMonth('payment_date'))
-        all = due.union(payed, all=True)
+            )
 
-        return transactions.\
-                annotate(date=TruncMonth('due_date')).\
-                values('date').\
-                annotate(
-                    effective_expenses=sum_kind(HasKind.EXPENSE_KIND),
-                    effective_incomes=sum_kind(HasKind.INCOME_KIND),
-                    real_expenses=sum_kind_payed(HasKind.EXPENSE_KIND),
-                    real_incomes=sum_kind_payed(HasKind.INCOME_KIND),
-                    effective_total=Sum('value')
-                ).\
-                order_by('date')
+        due_result = self._sum_queryset('due_date', due)
+        payed_result = self._sum_queryset('payment_date', payed)
+
+        return due_result.union(payed_result).order_by('date')        
+
+    def _sum_queryset(self, field, queryset):
+        sum_when = lambda **kwargs : Coalesce(Sum(Case(When(then=F('value'), **kwargs), default=0)), 0)
+        sum_kind = lambda kind : sum_when(kind=kind, due_date__month=ExtractMonth('date'), due_date__year=ExtractYear('date'))
+        sum_kind_payed = lambda kind : sum_when(kind=kind, payment_date__month=ExtractMonth('date'), payment_date__year=ExtractYear('date'))
+
+        return queryset.annotate(date=TruncMonth(field))\
+            .values('date').\
+            annotate(
+                effective_expenses=sum_kind(HasKind.EXPENSE_KIND),
+                effective_incomes=sum_kind(HasKind.INCOME_KIND),
+                real_expenses=sum_kind_payed(HasKind.EXPENSE_KIND),
+                real_incomes=sum_kind_payed(HasKind.INCOME_KIND),
+                effective_total=Sum('value')
+            )            
 
     def get_start_date(self):
         today = datetime.today()
