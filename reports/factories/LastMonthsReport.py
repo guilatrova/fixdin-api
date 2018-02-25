239cc0f99b1b0556f00d47adf75b00a5f9f40612
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-02-24 22:39:00 -0300

    Refactor

--- reports/factories/LastMonthsReport.py
+++ reports/factories/LastMonthsReport.py
@@ -23,6 +23,54 @@ class LastMonthsReportFactory:
 
         return report
 
+    def get_start_date(self):
+        today = datetime.today()
+        relative = today + relativedelta(months=-self.months)
+        return datetime(relative.year, relative.month, 1)
+
+    def get_end_date(self):
+        today = datetime.today()
+        last_day = calendar.monthrange(today.year, today.month)[1]
+        return datetime(today.year, today.month, last_day)
+
+    def _get_query(self):
+        start_date = self.get_start_date()
+        
+        transactions = Transaction.objects.filter(
+            Q(account__user_id=self.user_id),
+            Q(due_date__gte=start_date) | Q(payment_date__gte=start_date)
+        )
+        due = transactions
+        payed = transactions.filter(payment_date__isnull=False)\
+            .exclude(
+                payment_date__month=ExtractMonth('due_date'),
+                payment_date__year=ExtractYear('due_date')
+            )
+
+        due_result = self._sum_queryset('due_date', due)
+        payed_result = self._sum_queryset('payment_date', payed)
+
+        return due_result.union(payed_result).order_by('date')        
+
+    def _sum_queryset(self, field, queryset):
+        sum_when = lambda **kwargs : Coalesce(Sum(Case(When(then=F('value'), **kwargs), default=0)), 0)
+        sum_effective = lambda **kwargs : sum_when(**kwargs, due_date__month=ExtractMonth('date'), due_date__year=ExtractYear('date'))
+        sum_real = lambda **kwargs : sum_when(**kwargs, payment_date__isnull=False, payment_date__month=ExtractMonth('date'), payment_date__year=ExtractYear('date'))
+        sum_effective_kind = lambda kind : sum_effective(kind=kind)
+        sum_real_kind = lambda kind : sum_real(kind=kind)
+
+        #TruncMonth cares about year too
+        return queryset.annotate(date=TruncMonth(field))\
+            .values('date')\
+            .annotate(
+                effective_expenses=sum_effective_kind(HasKind.EXPENSE_KIND),
+                effective_incomes=sum_effective_kind(HasKind.INCOME_KIND),
+                real_expenses=sum_real_kind(HasKind.EXPENSE_KIND),
+                real_incomes=sum_real_kind(HasKind.INCOME_KIND),
+                effective_total=sum_effective(),
+                real_total=sum_real()
+            )
+
     def _merge_union(self, data):
         merged = []
         for row in data:
@@ -38,9 +86,10 @@ class LastMonthsReportFactory:
 
         return merged
 
-    def _sum_dict(self, lst):
+    @staticmethod
+    def _sum_dict(dicts):
         ret = defaultdict(int)
-        for d in lst:
+        for d in dicts:
             for k, v in d.items():
                 if (isinstance(v, date)):
                     ret[k] = v
@@ -65,51 +114,3 @@ class LastMonthsReportFactory:
         
         assert len(expected_dates) == len(data), \
             "Amount of rows should match amount of expected periods. Periods: {} Rows: {}".format(len(expected_dates), len(data))
-
-    def _get_query(self):
-        start_date = self.get_start_date()
-
-        #TruncMonth cares about year too
-        transactions = Transaction.objects.filter(
-            Q(account__user_id=self.user_id),
-            Q(due_date__gte=start_date) | Q(payment_date__gte=start_date)
-        )
-        due = transactions
-        payed = transactions.filter(payment_date__isnull=False)\
-            .exclude(
-                payment_date__month=ExtractMonth('due_date'),
-                payment_date__year=ExtractYear('due_date')
-            )
-
-        due_result = self._sum_queryset('due_date', due)
-        payed_result = self._sum_queryset('payment_date', payed)
-
-        return due_result.union(payed_result).order_by('date')        
-
-    def _sum_queryset(self, field, queryset):
-        sum_when = lambda **kwargs : Coalesce(Sum(Case(When(then=F('value'), **kwargs), default=0)), 0)
-        sum_effective = lambda **kwargs : sum_when(**kwargs, due_date__month=ExtractMonth('date'), due_date__year=ExtractYear('date'))
-        sum_real = lambda **kwargs : sum_when(**kwargs, payment_date__isnull=False, payment_date__month=ExtractMonth('date'), payment_date__year=ExtractYear('date'))
-        sum_kind = lambda kind : sum_effective(kind=kind)
-        sum_kind_payed = lambda kind : sum_real(kind=kind)
-
-        return queryset.annotate(date=TruncMonth(field))\
-            .values('date')\
-            .annotate(
-                effective_expenses=sum_kind(HasKind.EXPENSE_KIND),
-                effective_incomes=sum_kind(HasKind.INCOME_KIND),
-                real_expenses=sum_kind_payed(HasKind.EXPENSE_KIND),
-                real_incomes=sum_kind_payed(HasKind.INCOME_KIND),
-                effective_total=sum_effective(),
-                real_total=sum_real()
-            )            
-
-    def get_start_date(self):
-        today = datetime.today()
-        relative = today + relativedelta(months=-self.months)
-        return datetime(relative.year, relative.month, 1)
-
-    def get_end_date(self):
-        today = datetime.today()
-        last_day = calendar.monthrange(today.year, today.month)[1]
-        return datetime(today.year, today.month, last_day)
