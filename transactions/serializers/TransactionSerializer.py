64466a3b6c3e9ffe802348c315bb3e632ce42058
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-03-20 15:52:40 -0300

    Refactor transaction serializer

--- transactions/serializers/TransactionSerializer.py
+++ transactions/serializers/TransactionSerializer.py
@@ -5,7 +5,38 @@ from rest_framework.utils.serializer_helpers import ReturnDict, ReturnList
 from .PeriodicSerializer import PeriodicSerializer
 from .HasKindContextSerializer import HasKindContextSerializer
 
-class TransactionSerializer(serializers.ModelSerializer, HasKindContextSerializer):
+class SerializerMayReturnListMixin:
+    @property
+    def data(self):
+        if hasattr(self, 'initial_data') and not hasattr(self, '_validated_data'):
+            msg = (
+                'When a serializer is passed a `data` keyword argument you '
+                'must call `.is_valid()` before attempting to access the '
+                'serialized `.data` representation.\n'
+                'You should either call `.is_valid()` first, '
+                'or access `.initial_data` instead.'
+            )
+            raise AssertionError(msg)
+
+        if not hasattr(self, '_data'):
+            if self.instance is not None and not getattr(self, '_errors', None):
+                self._data = self.to_representation(self.instance)
+            elif hasattr(self, '_validated_data') and not getattr(self, '_errors', None):
+                self._data = self.to_representation(self.validated_data)
+            else:
+                self._data = self.get_initial()        
+        
+        if hasattr(self, '_validated_data') and self.is_return_data_list(self._validated_data):
+            return ReturnList(self._data, serializer=self)
+        return ReturnDict(self._data, serializer=self)
+
+    def to_representation(self, value):
+        if isinstance(value, list):
+            return [super(SerializerMayReturnListMixin, self).to_representation(x) for x in value]
+
+        return super(SerializerMayReturnListMixin, self).to_representation(value)
+
+class TransactionSerializer(SerializerMayReturnListMixin, serializers.ModelSerializer, HasKindContextSerializer):
     class Meta:
         model = Transaction
         fields = ('id', 'due_date', 'description', 'category', 'value', 'kind', 'details', 'account', 'priority', 'deadline', 'payment_date', 'periodic', 'bound_transaction', 'bound_reason')
@@ -47,35 +78,8 @@ class TransactionSerializer(serializers.ModelSerializer, HasKindContextSerialize
                 raise serializers.ValidationError('Transaction and Category must have the same kind')
         return data
 
-    def to_representation(self, value):
-        if isinstance(value, list):
-            return [super(TransactionSerializer, self).to_representation(x) for x in value]
-
-        return super(TransactionSerializer, self).to_representation(value)
-
-    @property
-    def data(self): #TODO: move this to a mixin or something else later
-        if hasattr(self, 'initial_data') and not hasattr(self, '_validated_data'):
-            msg = (
-                'When a serializer is passed a `data` keyword argument you '
-                'must call `.is_valid()` before attempting to access the '
-                'serialized `.data` representation.\n'
-                'You should either call `.is_valid()` first, '
-                'or access `.initial_data` instead.'
-            )
-            raise AssertionError(msg)
-
-        if not hasattr(self, '_data'):
-            if self.instance is not None and not getattr(self, '_errors', None):
-                self._data = self.to_representation(self.instance)
-            elif hasattr(self, '_validated_data') and not getattr(self, '_errors', None):
-                self._data = self.to_representation(self.validated_data)
-            else:
-                self._data = self.get_initial()        
-        
-        if hasattr(self, '_validated_data') and 'periodic' in self._validated_data:
-            return ReturnList(self._data, serializer=self)
-        return ReturnDict(self._data, serializer=self)
+    def is_return_data_list(self, validated_data):
+        return 'periodic' in validated_data    
 
     def create(self, validated_data):
         if 'periodic' in validated_data:            
