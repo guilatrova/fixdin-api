d64c88561515833a98cc95975aa4e0862d2389b9
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-04-15 11:20:59 -0300

    Removed useless functions

--- balances/queries.py
+++ balances/queries.py
@@ -6,34 +6,6 @@ from transactions.models import Transaction, Account, HasKind
 
 sum_when = lambda **kwargs : Coalesce(Sum(Case(When(then=F('value'), **kwargs), default=0)), 0) #TODO: move it somewhere else
 
-def get_total_pending_incomes(user_id):
-    today = date.today()
-    return Transaction.objects\
-        .owned_by(user_id)\
-        .pending()\
-        .incomes()\
-        .filter(due_date__lte=today)\
-        .aggregate(Sum('value'))['value__sum']
-
-def get_total_pending_expenses(user_id):
-    today = date.today()
-    return Transaction.objects\
-        .owned_by(user_id)\
-        .pending()\
-        .expenses()\
-        .filter(due_date__lte=today)\
-        .aggregate(Sum('value'))['value__sum']
-
-def get_accumulated_detailed(user_id, from_date, to_date):
-    return Transaction.objects\
-        .owned_by(user_id)\
-        .expires_between(from_date, to_date)\
-        .aggregate(
-            incomes=sum_when(kind=HasKind.INCOME_KIND),
-            expenses=sum_when(kind=HasKind.EXPENSE_KIND),
-            total=Sum('value')
-        )
-
 # TODO: Effective or not should be a query param
 def get_effective_incomes_expenses_by_account(user_id):
     start, end = get_start_end_month(date.today())
